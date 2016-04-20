a9f0d442772a8cb140fee3a4f5674f1f3bd0f464
Tom White <tom.white@methodsdigital.co.uk>
2016-04-20 15:17:47 +0100

    Add custom user with org membership

--- lanes/models.py
+++ lanes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 
 class Organisation(models.Model):
@@ -10,6 +11,19 @@ class Organisation(models.Model):
   def __unicode__(self):
     return self.name
 
+class User(AbstractUser):
+  organisations = models.ManyToManyField(Organisation, through='OrgMembership')
+
+class OrgMembership(models.Model):
+  ADMIN = 0
+  USER = 1
+  ROLES = (
+    (ADMIN, 'Admin'),
+    (USER, 'User')
+  )
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
+  role = models.IntegerField(choices=ROLES, default=USER)
 
 class Room(models.Model):
   name = models.CharField(max_length=100)
