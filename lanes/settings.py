4988424036f484973e5d1986a9ba7491d1e80610
Tom White <tom.white@methodsdigital.co.uk>
2016-05-04 13:28:21 +0100

    Update static settings

--- lanes/settings.py
+++ lanes/settings.py
@@ -31,7 +31,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 SESSION_ENGINE = 'redis_sessions.session'
 
